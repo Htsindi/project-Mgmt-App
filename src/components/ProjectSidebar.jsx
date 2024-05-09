77a438897b34ff5a7dc743dff1bbe1465afefc04
Martin Tsindi <matefortechnology@gmail.com>
2024-05-09 13:20:33 +0000

    Button and NoProjectSelected components added

--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 export default function ProjectSidebar() {
   return (
     <aside className="w-1/3 py-16 px-8 bg-stone-900 text-stone-50 md: w-72 rounded-r-xl">
@@ -5,9 +7,7 @@ export default function ProjectSidebar() {
         My Projects
       </h2>
       <div>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-100">
-          + Add Project
-        </button>
+        <Button>+ Add Project</Button>
       </div>
       <ul></ul>
     </aside>
